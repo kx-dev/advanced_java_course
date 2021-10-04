9ca369ef894cc34ee8aaefd757644d62ea2e9c5d
Dmitry Kravtsov <example@email.com>
2021-10-04 12:26:07 +0300

    After hard debug. Everything seems to work correctly.

---
+++ src/main/java/my/garage/Owner.java
@@ -0,0 +1,54 @@
+package my.garage;
+
+public class Owner {
+
+    private final long ownerId;
+    private final String name;
+    private final String lastName;
+    private final int age;
+
+    public Owner(long ownerId, String name, String lastName, int age) {
+        this.ownerId = ownerId;
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Owner{" +
+                "ownerId=" + ownerId +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        return this.ownerId == ((Owner) o).ownerId;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int)ownerId;
+    }
+
+    public long getOwnerId() {
+        return ownerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
